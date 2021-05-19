0f16b633815152de245abf5dda4a31175089a468
zhujiangtao01 <zhujiangtao01@bilibili.com>
2021-05-19 09:36:58 +0800

    删除KtFragment无关引用
    Signed-off-by: zhujiangtao01 <zhujiangtao01@bilibili.com>

--- StartModePro/user/src/main/java/com/zjt/user/KtFragment.kt
+++ StartModePro/user/src/main/java/com/zjt/user/KtFragment.kt
@@ -18,7 +18,6 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.zjt.base.BaseFragment
