9d12176d9245868632f919f8ece52f4e3cb1635e
zhujiangtao01 <zhujiangtao01@bilibili.com>
2021-05-28 09:40:38 +0800

    test2 的第3次提交
    Signed-off-by: zhujiangtao01 <zhujiangtao01@bilibili.com>

--- StartModePro/app/src/main/java/com/zjt/startmodepro/TestPermissionActivity.kt
+++ StartModePro/app/src/main/java/com/zjt/startmodepro/TestPermissionActivity.kt
@@ -65,6 +65,7 @@ class TestPermissionActivity : AppCompatActivity() {
                     //ctex21
                     // test2 分支第一次提交
                     // test2 分支的第二次提交
+                    // test2 分支的第3次提交
 //                    requestCameraAndCheckNotReminder()
                     val a = 11
 //                    requestStorageAndCheckNotReminder()
