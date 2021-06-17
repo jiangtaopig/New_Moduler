36ae032d893cc443503ad929ee3667b25e01735d
zhujiangtao01 <zhujiangtao01@bilibili.com>
2021-06-17 11:32:15 +0800

    提交1
    Signed-off-by: zhujiangtao01 <zhujiangtao01@bilibili.com>

---
+++ StartModePro/app/src/main/kotlin/com/zjt/startmodepro/ConstantV2.kt
@@ -0,0 +1,16 @@
+package com.zjt.startmodepro
+
+/**
+ * top level 属性，调用直接 写 PORTRAIT_PRE_VIEW_SHARE_ID，不能写成 ConstantV2.PORTRAIT_PRE_VIEW_SHARE_ID
+ */
+const val PORTRAIT_PRE_VIEW_SHARE_ID = "live.my-live-camera.0.0.pv" // 竖屏 播前 id
+const val PORTRAIT_PRE_VIEW_SHARE_ORIGIN = "horizontal-share"
+
+const val PORTRAIT_LIVING_SHARE_ID = "live.my-live-room-show.0.0.pv" // 竖屏 播中 id
+const val PORTRAIT_LIVING_SHARE_ORIGIN = "horizontal-share"
+
+const val LANDSCAPE_PRE_VIEW_SHARE_ID = "live.my-live-camera.0.0.pv" //  横屏 播前 id
+const val LANDSCAPE_PRE_VIEW_SHARE_ORIGIN = "vertical-share"
+
+const val LANDSCAPE_LIVING_SHARE_ID = "live.my-live-room-show.0.0.pv" //  横屏 播中 id
+const val LANDSCAPE_LIVING_SHARE_ORIGIN = "vertical-share"
